4eb11ade2fd974557bfec5452a8cd75d51536e2c
hugomacielads <hugomacielcesar@gmail.com>
2021-09-15 01:24:28 +0000

    (14/09) Criação do projeto React, Links e Routes, usuEffect, localStorage

---
+++ react-hooks/src/exercicios/Teste1.js
@@ -0,0 +1,8 @@
+export default function Teste1() {
+    return (
+        <>
+            <h1>Primiero componente React</h1>
+            <p>Este é o primiero componenete React.</p>
+        </>
+    )
+}
